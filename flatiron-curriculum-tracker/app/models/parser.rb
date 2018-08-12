337f2313b15c4e5844a5e7afe5943c03c664f080
veloblank <veloblank@gmail.com>
2018-08-12 10:13:03 -0400

    Create JSON file that can be parsed for data

--- flatiron-curriculum-tracker/app/models/parser.rb
+++ flatiron-curriculum-tracker/app/models/parser.rb
@@ -1,6 +1,18 @@
+require 'open-uri'
+require 'pry'
+require 'json'
+
 class Parser
+  attr_accessor :source
 
-  
+  def initialize
+    source = File.read(File.dirname(__FILE__) + '/curriculum-source.json')
+    @source = JSON.parse(source)
+  end
 
+  def self.regexr(source)
+
+  end
+binding.pry
 
 end
--- flatiron-curriculum-tracker/app/models/unit.rb
+++ flatiron-curriculum-tracker/app/models/unit.rb
@@ -1,5 +1,24 @@
+require_relative './parser.rb'
 class Unit
+  attr_accessor :name, :completed?, :slug
 
+  def intialize(name, completed, slug)
+    @name = name
+    @completed = completed
+    @slug = slug
+  end
+
+  def unit_builder
+    curriculum = Parser.new
+    units = curriculum.flatten
+    units.each do |u|
+      unit_name = u[:title]
+      complete = u[:complete]
+      slug = u[:slug]
+      @units.push({name: unit_name, completed: complete, slug: slug})
+    end
+    self
+  end
 
 
 end
